74d198c2c76f16fa1cdc6afd1362dfefe1243939
CrimsonProtostar <5025211155@mhs.its.ac.id>
2023-10-21 23:05:37 +0700

    ADD : Proyek ThreeJs hit detection

---
+++ Proyek-ThreeJS/notes.md
@@ -0,0 +1,11 @@
+Sudah :
+- Deteksi collision antara 2 objek
+
+TODO :
+
+- [x] Kelas khusus untuk player 
+- [ ] Kelas khusus untuk DeathBox (Jika tabrakan dengan DeathBox, player mati)
+- [ ] Kelas khusus untuk scene threejs (untuk kemampuan restart level)
+- [ ] Bisa menambah lebih dari 1 entitas DeathBox
+- [ ] Ganti player model + tambah animasi pakai mixamo
+- [ ] Player bisa bergerak 3D (tidak hanya 2D) mengikuti rute tertentu dan tidak pergi dari rute tersebut.
--- Proyek-ThreeJS/projek-threejs-astra-impossiblegame/main.js
+++ Proyek-ThreeJS/projek-threejs-astra-impossiblegame/main.js
@@ -7,11 +7,110 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 const chunkX = 12
 const chunkZ = 2
 
+/**
+ * Class for ThreeJs objects using a box body.
+ * Parameter :
+ * - Mesh : ThreeJs mesh, represents the object.
+ * - halfExtents : CANNON.Vec3, represents HALF of the object's size.
+ * - isStatic : True if the object doesn't move.
+ * - initPos : CANNON.Vec3, Initial Position of the Object.
+ */
+class activeObject {
+  constructor(Mesh, halfExtents, isStatic, initPos) {
+    this.MESH = Mesh
+    this.HALF_EXTENTS = halfExtents
+
+    // Setup initial position
+    this.objectShape = new CANNON.Box(this.HALF_EXTENTS)
+    this.objectBody = new CANNON.Body({ mass: 2, shape: this.objectShape })
+    this.MESH.position.copy(initPos)
+    this.objectBody.position.copy(initPos)
+    scene.add(Mesh)
+    world.addBody(this.objectBody)
+  }
+
+  /**
+   * Updates object position (called in animate() function) or changes it based on "Pos".
+   * @param {CANNON.Vec3|THREE.Vector3} [Pos=undefined] Pos - Where the object is moved
+   */
+  updatePosition(Pos) {
+    let nextPosition
+    if (Pos === undefined) {
+      nextPosition = this.objectBody.position
+    }
+    else {
+      nextPosition = Pos
+    }
+    this.MESH.position.copy(nextPosition)
+    this.objectBody.position.copy(nextPosition)
+    this.MESH.quaternion.copy(this.objectBody.quaternion)
+  }
+
+}
+
+class playerObject extends activeObject {
+  constructor(initPos) {
+    const size = 1
+    const geometry = new THREE.BoxGeometry(size, size, size)
+    const material = new THREE.MeshStandardMaterial({ color: 0xffff00 })
+    const player = new THREE.Mesh(geometry, material)
+    player.castShadow = true
+    super(player, new CANNON.Vec3(size / 2, size / 2, size / 2), 0, initPos)
+
+    // Player controls
+    this.spacebar_pressed = false
+    this.setControl()
+
+    // Player status
+    this.isDead = false
+
+  }
+  
+  setControl() {
+    // CONTROLS
+    // https://stackoverflow.com/questions/70111463/method-not-defined-when-calling-from-inside-an-event-listener
+    document.addEventListener("keydown", (e) => {
+      if (e.key == ' ') {
+        e.preventDefault();
+        this.spacebar_pressed = true;
+        this.updateControl()
+        console.log("JUMP");
+      };
+    })
+    document.addEventListener("keyup", (e) => {
+      if (e.key == ' ') {
+        this.spacebar_pressed = false;
+      };
+    })
+  }
+
+  updatePosition(Pos) {
+    super.updatePosition(Pos)
+    // MOVEMENT
+    this.objectBody.velocity.x = 5 // Kecepatan dasar
+    
+    this.objectBody.position.z = 0
+
+    this.objectBody.quaternion.x = 0
+    this.objectBody.quaternion.y = 0
+    this.objectBody.quaternion.z = 0
+  }
+
+  updateControl() {
+    if (this.spacebar_pressed && this.objectBody.position.y < 1) {
+      this.objectBody.velocity.y = 0
+      this.objectBody.applyImpulse(new CANNON.Vec3(0, 35, 0))
+      this.spacebar_pressed = false
+    }
+  }
+
+}
+
 const scene = new THREE.Scene()
 /* INIT PHYSICS */
 
 const world = new CANNON.World({
-  gravity: new CANNON.Vec3(0, -9.82, 0), // m/s²
+  gravity: new CANNON.Vec3(0, -9.82*5, 0), // m/s²
 })
 
 // Sets up camera, renderer, & camera controls
@@ -33,44 +132,35 @@ camera.position.z = 5
 
 // Sets up objects
 // 1. Player
-const size = 1
-const initPos = new CANNON.Vec3(0, 0, 0)
-const geometry = new THREE.BoxGeometry(size, size, size)
-const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 })
-const player = new THREE.Mesh(geometry, material)
-player.castShadow = true
-scene.add(player)
+let player = new playerObject(new CANNON.Vec3(0, 0, 0))
 /* PLAYER PHYSICS */
-const halfExtents = new CANNON.Vec3(size / 2, size / 2, size / 2)
-const playerShape = new CANNON.Box(halfExtents)
-const playerBody = new CANNON.Body({ mass: 2, shape: playerShape })
-playerBody.position = initPos
-
-// CONTROLS
-var spacebar_pressed = false;
-document.addEventListener("keydown", function(e) {
-  if (e.key == ' ') {
-      e.preventDefault();
-      spacebar_pressed = true;
-      console.log("JUMP");
-  };
-})
-document.addEventListener("keyup", function(e) {
-  if (e.key == ' ') {
-      spacebar_pressed = false;
-  };
-})
 
 
-world.addBody(playerBody)
+
+
 // 2. Level Generation
 
 
 /* PLACEHOLDER FLOOR */
-generateFloor()
+let groundRef = generateFloor()
 
 // Sets up lighting
 light()
+
+/* Collision detection (PLACEHOLDER) 
+* Checks world's collision matrix (contains data on collisions) for player colliding with deathBox
+*/
+const size = 1
+const geometry = new THREE.BoxGeometry(size, size, size)
+const material = new THREE.MeshStandardMaterial({ color: 0xffff00 })
+const debugMesh = new THREE.Mesh(geometry, material)
+let debugBox = new activeObject(debugMesh, new CANNON.Vec3(0.5, 0.5, 0.5), 0, new CANNON.Vec3(10, 5, 0))
+player.objectBody.addEventListener("collide", function (e) {
+  if (world.collisionMatrix.get(player.objectBody, debugBox.objectBody)) {
+    console.log("HOW")
+  }
+})
+
 
 // Timer for updating animation
 const direction = new THREE.Vector3();
@@ -82,21 +172,14 @@ function animate() {
   world.fixedStep()
 
   /* OBJECT ANIMS + PHYSICS */
-  // Player
-
-  playerBody.velocity.x = 3
-  if(spacebar_pressed) {
-    
-    playerBody.applyImpulse(new CANNON.Vec3(0, 1, 0))
-  }
-  player.position.copy(playerBody.position)
-  player.quaternion.copy(playerBody.quaternion)
-  controls.target = player.position
-  controls.update()
+  player.updatePosition()
+  debugBox.updatePosition()
   /* CAMERA FOLLOW PLAYER */
-  direction.subVectors( camera.position, controls.target );
-  direction.normalize().multiplyScalar( 20 );
-  camera.position.copy( direction.add( controls.target ) );
+  controls.target = player.MESH.position
+  controls.update()
+  direction.subVectors(camera.position, controls.target);
+  direction.normalize().multiplyScalar(20);
+  camera.position.copy(direction.add(controls.target));
 
 }
 animate()
@@ -123,13 +206,16 @@ function generateFloor() {
   const groundBody = new CANNON.Body({
     type: CANNON.Body.STATIC,
     shape: new CANNON.Plane(),
+
   })
+
   groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0) // make it face up
   floor.position.copy(groundBody.position)
   floor.quaternion.copy(groundBody.quaternion)
   scene.add(floor);
   world.addBody(groundBody)
   console.log(floor.position)
+  return groundBody
 }
 function wrapAndRepeatTexture(map) {
   map.wrapS = map.wrapT = THREE.RepeatWrapping;
